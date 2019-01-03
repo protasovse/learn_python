33745eecea7d9919180d6b4da7a6dc1bd84dbb3f
protasovse <protasovse@gmail.com>
2019-01-03 18:14:19 +0500

    before dict

---
+++ buffet.py
@@ -0,0 +1,10 @@
+
+foods = ('red caviar with red caviar', 'hot smoked sturgeon', 'smoked eel',
+         'norwegian salmon marinated in herbs', 'crayfish Rolls with Avocado and Trout')
+
+foods = ('red caviar with red caviar', 'hot smoked sturgeon', 'smoked eel',
+         'apples', 'eggs')
+
+print('\n======================\n')
+for food in foods:
+    print(food.title())
--- cars.py
+++ cars.py
@@ -1,25 +1,26 @@
 cars = ['bmw', 'audi', 'toyota', 'subaru']
 
-print("Here is the orginal list: ")
-print(cars)
-
-print("\nHere is the sorted list: ")
-print(sorted(cars))
-
-print("\nHere is the orginal list again: ")
-print(cars)
+# print("Here is the orginal list: ")
+# print(cars)
+#
+# print("\nHere is the sorted list: ")
+# print(sorted(cars))
+#
+# print("\nHere is the orginal list again: ")
+# print(cars)
 
 cars.reverse()
-print(cars)
+# print(cars)
 
 l = len(cars)
 
-print(l)
+# print(l)
 
 
+cars = ['bmw', 'audi', 'toyota', 'subaru']
 
-
-# cars.sort()
-# print(cars)
-# cars.sort(reverse=True)
-# print(cars)
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.title())
