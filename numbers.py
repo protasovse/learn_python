c4cc958c9b3ea4822181a510010f3308e5c63aa9
protasovse <protasovse@gmail.com>
2019-01-03 15:07:12 +0500

    initial

---
+++ numbers.py
@@ -0,0 +1,16 @@
+for x in range(1, 21):
+    print(x)
+
+million = list(range(1, 1000001))
+# file = open('./million.txt', 'w')
+# file.write(', '.join([str(x) for x in million]))
+# file.close()
+
+# numbers = list(range(2, 11, 2))
+# squares = [x**x for x in range(1, 11)]
+# print(squares)
+print(min(million))
+print(max(million))
+print(sum(million))
+##
+
